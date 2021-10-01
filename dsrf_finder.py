1756ac0ff880e59ea512e743815e65acb9d2fb36
Anas bin hasan bhuiyan <anashasan8523@gmail.com>
2021-10-01 23:45:58 +0600

    py update

--- dsrf_finder.py
+++ dsrf_finder.py
@@ -9,11 +9,21 @@ from requests_html import HTMLSession
 
 #function 1
 
-def hfinder(link, path):
+""" 
+!no need to write this for xpath_hfinder
+
+session = HTMLSession()
+    #r = session.get(link)
+    #data = r.html.xpath(xpath) (if path is xpath)
+#path = 'any path css or xpath
+
+"""
+
+def xpath_hfinder(link, xpath):
 
     session = HTMLSession()
     r = session.get(link)
-    x = r.html.xpath(path)
+    x = r.html.xpath(xpath)
     z = []
     k = []
     for item in (x):
@@ -34,9 +44,51 @@ def hfinder(link, path):
 
 #function 2
 
-def phfinder(x, y):
-    x = x(y)
-    
+""" 
+# !no need to write this for css_hfinder
+
+session = HTMLSession()
+    r = session.get(link)
+    data = r.html.find(css_path) (if path is css)
+path = 'any css path'
+
+"""
+
+def css_hfinder(link, css_path):
+    session = HTMLSession()
+    r = session.get(link)
+    x = r.html.find(css_path)
+    z = []
+    k = []
+    for item in (x):
+        try:
+            x = item.text
+            z.append(x)
+        except Exception as e:
+            x = item
+            z.append(x)
+        try:
+            x = item.attrs['href']
+            k.append(x)
+        except Exception as e:
+            x = None
+            k.append(x)
+    return z, k
+
+
+
+#function 3
+
+"""session = HTMLSession()
+   r = session.get(link)
+   data = r.html.find(css_path) (if path is css)
+or,
+   data = r.html.xpath(xpath) (if path is xpath)
+path = 'any path css or xpath'
+"""
+
+def phfinder(data, path):
+    x = data(path)
     z = []
     k = []
     for item in (x):
